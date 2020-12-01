263772c835d74c04d8e98df32397b49ef91c1b92
guillaume verpoest <guillaume.verpoest@isen-ouest.yncrea.fr>
2020-12-01 12:13:01 +0100

    test

--- source/query.py
+++ source/query.py
@@ -8,14 +8,47 @@ class Query():
 
     def get_all_lignes(self):
         self.cursor.execute("""SELECT nom FROM lignes""")
-        print(self.cursor.fetchall())
+        return(self.cursor.fetchall())
 
     def get_arrets_for_ligne(self, ligne):
+        arret_ligne = {}
         self.cursor.execute("""SELECT nom  FROM arrets INNER JOIN arrets_lignes ON arrets.id_arret = arrets_lignes.id_arret WHERE id_ligne = (SELECT id_ligne FROM lignes WHERE nom = '%s')"""%(ligne))
-        print(self.cursor.fetchall())
+        arret_ligne["lignes"] = self.cursor.fetchall()
         self.cursor.execute("""SELECT numeros FROM bus INNER JOIN lignes ON lignes.id_ligne = bus.id_ligne WHERE lignes.nom = '%s'"""%(ligne))
-        print(self.cursor.fetchall())
+        arret_ligne["arrets"] = self.cursor.fetchall()
+        return arret_ligne
 
+    def add_bus(self, ligne, bus):
+        if self.is_existed_bus(bus):
+            if self.is_existed_ligne(ligne):
+                self.cursor.execute("""SELECT id_ligne FROM lignes WHERE nom = '%s'"""%(ligne))
+                id_ligne = self.cursor.fetchall()[0]
+                self.cursor.execute("""INSERT INTO bus(numeros ,id_ligne) VALUES ('%s', %d)"""%(bus, id_ligne[0]))
+                self.mydb.commit()
+                self.mydb.close()
+                return "le bus a bien ete ajouter"
+            else:
+                return "la ligne existe pas"
+        else:
+            return "le bus existe deja"
+    
+    def is_existed_bus(self, bus):
+        self.cursor.execute("""SELECT * FROM bus WHERE numeros = '%s'"""%(bus))
+        item = self.cursor.fetchall()
+        if not item: # le bus existe pas
+            return True
+        else: # la ligne existe
+            return False
 
-        
-Query().get_arrets_for_ligne('Noir')
+    def is_existed_ligne(self, ligne):
+        self.cursor.execute("""SELECT * FROM lignes WHERE nom = '%s'"""%(ligne))
+        item = self.cursor.fetchall()
+        if not item: # la ligne existe pas
+            return False
+        else: # la ligne existe
+            return True
+
+    def delete_bus(self, bus):
+        self.cursor.execute("""DELETE FROM bus WHERE numeros = '%s'"""%(bus))
+
+print(Query().add_bus("Noir","BB09"))
--- source/sandBox.py
+++ source/sandBox.py
@@ -1,1 +1,27 @@
+from tkinter import *
+from query import Query
 
+class Root:
+    def __init__(self):
+        self.root=Tk()
+        self.root.title =("titre")
+        self.root.geometry("900x1130")
+        self.root.minsize(480, 360)
+        self.root.config(background="white")
+
+        self.compute()
+
+    def compute(self):
+        self.display_ligne()
+
+    def a(self, text):
+        print(text)
+
+    def display_ligne(self):
+        var1= IntVar()
+        ligne_of_bus = [('Rouge',), ('Vert',), ('Bleu',), ('Noir',)]
+        frame_ligne = Frame(self.root).pack()
+        for i in ligne_of_bus:
+            Label(frame_ligne, )
+
+Root().root.mainloop()
